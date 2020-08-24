71e329a33ba03b18e91a2626f3c2381c07fade95
David Kiv <funzeye@gmail.com>
2020-08-24 17:29:34 +0100

    updating website title

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
-    : '/'
+    : '/',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'My Shout'
+        return args
+      })
+  }
 }
 
 // used if using github pages with default domain
